251f37474c50819f9ed9629d39e7c2d8aaca24c9
Thayano Davalo <ttdavalo@gmail.com>
2025-06-01 16:38:27 -0300

    conclusao do projeto

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,15 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+export const experimental_ppr = true;
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,7 +21,7 @@ export async function fetchRevenue() {
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -161,6 +161,7 @@ export async function fetchInvoiceById(id: string) {
     }));
 
     return invoice[0];
+    
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch invoice.');
